c482c5a40cd4acddef0b6ecac29cc6993492d311
yaoshaohua <yaoshaohua@xinye.com>
2021-02-23 11:07:09 +0800

    update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path = require('path')
 export default defineConfig({
-  alias: {
-    '/@': path.resolve(__dirname, './src')
+  resolve: {
+    alias: {
+      '/@': path.resolve(__dirname, './src')
+    }
   },
   optimizeDeps: {
     include: ['@ant-design/icons-vue', '@ant-design-vue/use']
